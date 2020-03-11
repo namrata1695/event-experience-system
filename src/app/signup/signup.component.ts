ebf36acb4f78f6e733a25d27ddd852b371bb6d6d
namrata1695 <ajmerin@uwindsor.ca>
2020-03-10 23:52:07 -0400

    change in UI

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'app-signup',
-    templateUrl : './signup.component.html'
+    templateUrl : './signup.component.html',
+    styles : [`
+    a
+    {
+        color: #fff;
+    }
+    `]
 })
 export class SignupComponent 
 {
