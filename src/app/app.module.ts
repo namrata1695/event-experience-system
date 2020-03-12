982c79a7da563f0ea2a466cec64fe6b9f2bde378
namrata1695 <ajmerin@uwindsor.ca>
2020-03-12 08:54:05 -0400

    building conponents and designing layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
 import {SignupComponent} from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    SignupComponent
+    SignupComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector : 'app-signup',
     templateUrl : './signup.component.html',
-    styles : [`
-    a
-    {
-        color: #fff;
-    }
-    `]
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent 
 {
